e6bb6b346bd241ee0b9c475b476595e8bfc6db86
A BHARGHAV RAM <bhargavram668@gmail.com>
2026-01-26 01:14:47 +0530

    Time: 55 ms (43.42%), Space: 70.5 MB (61.35%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,39 +1,20 @@
+import java.util.*;
+
 class Solution {
     public int totalFruit(int[] fruits) {
-        int left = 0, max = 0;
-        int type1 = -1, type2 = -1;
-        int count1 = 0, count2 = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int left = 0, ans = 0;
 
         for (int right = 0; right < fruits.length; right++) {
-            int f = fruits[right];
+            map.put(fruits[right], map.getOrDefault(fruits[right], 0) + 1);
 
-            if (f == type1) {
-                count1++;
-            } else if (f == type2) {
-                count2++;
-            } else if (type1 == -1) {
-                type1 = f;
-                count1 = 1;
-            } else if (type2 == -1) {
-                type2 = f;
-                count2 = 1;
-            } else {
-                max = Math.max(max, right - left);
-                while (count1 > 0 && count2 > 0) {
-                    int lf = fruits[left++];
-                    if (lf == type1) count1--;
-                    else count2--;
-                }
-                if (count1 == 0) {
-                    type1 = f;
-                    count1 = 1;
-                } else {
-                    type2 = f;
-                    count2 = 1;
-                }
+            while (map.size() > 2) {
+                map.put(fruits[left], map.get(fruits[left]) - 1);
+                if (map.get(fruits[left]) == 0) map.remove(fruits[left]);
+                left++;
             }
-            max = Math.max(max, right - left + 1);
+            ans = Math.max(ans, right - left + 1);
         }
-        return max;
+        return ans;
     }
 }
