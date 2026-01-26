8e6fff8ce806a0b519a89e93bfc2d6b57f266928
A BHARGHAV RAM <bhargavram668@gmail.com>
2026-01-26 21:47:46 +0530

    Time: 3 ms (63.4%), Space: 47.8 MB (44.85%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int dup = -1, miss = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            int idx = Math.abs(nums[i]) - 1;
+
+            if (nums[idx] < 0) {
+                dup = Math.abs(nums[i]);
+            } else {
+                nums[idx] = -nums[idx];
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                miss = i + 1;
+            }
+        }
+
+        return new int[]{dup, miss};
+    }
+}
