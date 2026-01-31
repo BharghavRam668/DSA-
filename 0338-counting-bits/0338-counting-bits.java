82397f7848b95eb2727ff03e7f90e1ccdb54f629
A BHARGHAV RAM <bhargavram668@gmail.com>
2026-01-31 21:16:04 +0530

    Time: 76 ms (5.53%), Space: 48.8 MB (44.03%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,11 +1,16 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] ans = new int[n + 1];
-
-        for (int i = 1; i <= n; i++) {
-            ans[i] = ans[i & (i - 1)] + 1;
+        int[] arr = new int[n+1];
+        
+        for(int i = 0 ; i < arr.length ; i++){
+            int count = 0;
+            int temp = i;
+            while(temp>0){
+                temp = temp & (temp-1);
+                count++;
+            }
+            arr[i] = count;
         }
-
-        return ans;
+        return arr;
     }
 }
