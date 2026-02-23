5cec6f0e2d30b6a10fc8a84d4fb43d4b8ab949e6
A BHARGHAV RAM <bhargavram668@gmail.com>
2026-02-23 16:34:24 +0530

    Time: 4 ms (7.25%), Space: 49.3 MB (10.91%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,32 +1,28 @@
 class Solution {
-        public List<String> letterCombinations(String digits) {
 
-                List<String> ans = new ArrayList<>();
-                        if (digits == null || digits.length() == 0) return ans;
+    ArrayList<String> result = new ArrayList<>();
+    String[] mapping = {
+        "" , "" , "abc" , "def" , "ghi" , "jkl" , "mno" , "pqrs" , "tuv" , "wxyz"
+    };
 
-                                String[] map = {
-                                            "", "", "abc", "def", "ghi",
-                                                        "jkl", "mno", "pqrs", "tuv", "wxyz"
-                                                                };
+    public List<String> letterCombinations(String digits){
+        
+        if(digits.length() == 0) return result;
+        
+        backtrack(0,digits,"");
+        
+        return result;
+    }
+    private void backtrack(int index , String digits , String currentString){
+        if(index == digits.length()){
+            result.add(currentString);
+            return;
+        }
+        int digit = digits.charAt(index) - '0' ;
+        String letters = mapping[digit];
 
-                                                                        Queue<String> q = new LinkedList<>();
-                                                                                q.offer("");
-
-                                                                                        for (int i = 0; i < digits.length(); i++) {
-
-                                                                                                    int size = q.size();
-                                                                                                                String letters = map[digits.charAt(i) - '0'];
-
-                                                                                                                            for (int s = 0; s < size; s++) {
-                                                                                                                                            String curr = q.poll();
-
-                                                                                                                                                            for (char ch : letters.toCharArray()) {
-                                                                                                                                                                                q.offer(curr + ch);
-                                                                                                                                                                                                }
-                                                                                                                                                                                                            }
-                                                                                                                                                                                                                    }
-
-                                                                                                                                                                                                                            ans.addAll(q);
-                                                                                                                                                                                                                                    return ans;
-                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                        }
+        for(char c : letters.toCharArray()){
+            backtrack(index+1 , digits , currentString + c);
+        }
+    }
+}                                                                                                                                                                                                                                                                                   
