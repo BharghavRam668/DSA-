494d3e9d40e8d43efaac3ed790aaadae080732cf
A BHARGHAV RAM <bhargavram668@gmail.com>
2026-04-14 00:19:52 +0530

    Time: 579 ms (11.32%), Space: 0B (100%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,13 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      SELECT salary
+          FROM Employee e1
+              WHERE N = (
+                    SELECT COUNT(DISTINCT e2.salary)
+                          FROM Employee e2
+                                WHERE e2.salary >= e1.salary
+                                    )
+                                        LIMIT 1
+                                          );
+                                          END;
