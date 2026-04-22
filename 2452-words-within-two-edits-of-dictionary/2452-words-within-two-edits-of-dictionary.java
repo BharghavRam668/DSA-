02e9d7b6fca4849928bb2f77b5f6eb6eab36999a
A BHARGHAV RAM <bhargavram668@gmail.com>
2026-04-22 21:47:25 +0530

    Time: 6 ms (72.79%), Space: 43.9 MB (99.26%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.java
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.java
@@ -1,26 +1,25 @@
 class Solution {
     public List<String> twoEditWords(String[] queries, String[] dictionary) {
-                    List<String> result = new ArrayList<>();
+         List<String> result = new ArrayList<>();
 
-                            for (String q : queries) {
-                                        for (String d : dictionary) {
-                                                        int diff = 0;
+                 for (String q : queries) {
+                             for (String d : dictionary) {
+                                             int diff = 0;
 
-                                                                        for (int i = 0; i < q.length(); i++) {
-                                                                                            if (q.charAt(i) != d.charAt(i)) {
-                                                                                                                    diff++;
-                                                                                                                                            if (diff > 2) break; // early stop
-                                                                                                                                                                }
-                                                                                                                                                                                }
+                                                             for (int i = 0; i < q.length(); i++) {
+                                                                                 if (q.charAt(i) != d.charAt(i)) {
+                                                                                                         diff++;
+                                                                                                                                 if (diff > 2) break; // early stop
+                                                                                                                                                     }
+                                                                                                                                                                     }
 
-                                                                                                                                                                                                if (diff <= 2) {
-                                                                                                                                                                                                                    result.add(q);
-                                                                                                                                                                                                                                        break; // no need to check other dictionary words
-                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                                    }
-                                                                                                                                                                                                                                                                            }
+                                                                                                                                                                                     if (diff <= 2) {
+                                                                                                                                                                                                         result.add(q);
+                                                                                                                                                                                                                             break; // no need to check other dictionary words
+                                                                                                                                                                                                                                             }
+                                                                                                                                                                                                                                                         }
+                                                                                                                                                                                                                                                                 }
 
-                                                                                                                                                                                                                                                                                    return result;
-         
+                                                                                                                                                                                                                                                                         return result;
     }
 }
